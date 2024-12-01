e1b24ab022bb22ea76a341de5248f53ebe67d335
xiamojixia <2206486730@qq.com>
2024-12-01 18:38:10 +0800

    P1 Person

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,63 @@
 public class Person {
+    private int id ;
+    private String name ;
+    private String gender ;
+    private String group ; //Child or adult
+    private String ticketType ; //Individual, family, or group
     
+    public Person(int id, String name, String gender, String group, String ticketType){
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.group = group;
+        this.ticketType = ticketType;
+    }
+    
+    public Person(){
+        this.id = 0;
+        this.name = "None";
+        this.gender = "None";
+        this.group = "None";
+        this.ticketType = "None";
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
 }
